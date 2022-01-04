e30e114a6026e77dcb50e2d965dc193686c5f7cc
Anton <lemonydarkwood@gmail.com>
2022-01-04 16:05:20 +0900

    Начало создание программы

--- Array/Program.cs
+++ Array/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Необходимо написать программу, которая из имеющегося массива целых чисел формирует массив из четных чисел. 
+// Первоначальный массив будет сгенерирован случайным образом.
+// CreateArray(int count);
+// FillArray(int[] array, int minValue, int maxValue);
+// PrintArray(int[] array);
+// EvenNumbers(int[] array);
+// Console.WriteLine(PrintArray(array)); 
+// Console.WriteLine(PrintArray(EvenNumbers(array))); 
+
