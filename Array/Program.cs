3b2398796ef9cc0111c3dfb9cd0cf29b86b9777c
Anton <lemonydarkwood@gmail.com>
2022-01-04 16:09:53 +0900

    Заполнение массив

--- Array/Program.cs
+++ Array/Program.cs
@@ -11,3 +11,13 @@ int[] CreateArray(int count)
 {
     return new int[count];
 }
+
+void FillArray(int[] array, int minValue, int maxValue)
+{
+    int count = array.Length;
+    for (int i = 0; i < count; i++)
+    {
+        array[i] = new Random().Next(minValue, maxValue);
+    }
+}
+
